b105ad7b30e34873f43ad24ebb4232873a2cf788
chandansingh2555-ship-it <chandansingh2555@gmail.com>
2026-03-10 14:43:46 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCiIpEt33eBLmna3YdXEbXZ4wLP5CLD20w",
+  authDomain: "wathanzi.firebaseapp.com",
+  databaseURL: "https://wathanzi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wathanzi",
+  storageBucket: "wathanzi.firebasestorage.app",
+  messagingSenderId: "456356745959",
+  appId: "1:456356745959:web:aeb1adaeba3cc73cbdc842"
+};
